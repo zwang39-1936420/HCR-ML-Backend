c81af0359d7b7743bf7f0fc63dd134fb8e165bd2
Eddie Wang <eddywongziyue@gmail.com>
2023-12-17 21:22:01 -0500

    final submission

--- final-project-backend/App.py
+++ final-project-backend/App.py
@@ -46,11 +46,17 @@ def predict():
         return jsonify({'error': str(e)})
 
 def sort_contours(cnts, method="left-to-right"):
-    reverse = method in ["right-to-left", "bottom-to-top"]
-    i = 1 if method in ["top-to-bottom", "bottom-to-top"] else 0
+    reverse = False
+    i = 0
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    cnts, boundingBoxes = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
-    return cnts, boundingBoxes
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+    key=lambda b:b[1][i], reverse=reverse))
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
 
 def convert_image_to_png(image):
     # Convert the OpenCV image to PIL Image
@@ -67,29 +73,27 @@ def get_letters(img):
     letters = []
     image = cv2.imdecode(np.frombuffer(img.read(), np.uint8), cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    ret,thresh1 = cv2.threshold(gray ,127,255,cv2.THRESH_BINARY_INV)
     dilated = cv2.dilate(thresh1, None, iterations=2)
 
-    cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sort_contours(cnts, method="left-to-right")[0]
-
+    # loop over the contours
     for c in cnts:
         if cv2.contourArea(c) > 10:
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
         roi = gray[y:y + h, x:x + w]
-        thresh = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        thresh = cv2.resize(thresh, (32, 32), interpolation=cv2.INTER_CUBIC)
+        thresh = cv2.threshold(roi, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        thresh = cv2.resize(thresh, (32, 32), interpolation = cv2.INTER_CUBIC)
         thresh = thresh.astype("float32") / 255.0
         thresh = np.expand_dims(thresh, axis=-1)
-        thresh = thresh.reshape(1, 32, 32, 1)
+        thresh = thresh.reshape(1,32,32,1)
         ypred = model.predict(thresh)
         ypred = LB.inverse_transform(ypred)
         [x] = ypred
         letters.append(x)
-    
     return letters, image
 
 def get_word(letter):
